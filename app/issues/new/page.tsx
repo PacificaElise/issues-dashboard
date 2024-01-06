a51c3f57c6331f3f8caa0b0029e20c8b0ed92932
Tatiana Ivanova <tati.ivanova.87@gmail.com>
2024-01-06 04:43:38 +0300

    feat: extract ErrorMessage component

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -10,6 +10,7 @@ import { useState } from 'react';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { createIssueSchema } from '@/app/validationSchema';
 import { z } from 'zod';
+import ErrorMessage from '@/app/components/ErrorMessage';
 
 type IssueForm = z.infer<typeof createIssueSchema>;
 
@@ -53,14 +54,7 @@ const NewIssuePage = () => {
             {...register('title')}
           />
         </TextField.Root>
-        {errors.title && (
-          <Text
-            color='red'
-            as='p'
-          >
-            {errors.title.message}
-          </Text>
-        )}
+        <ErrorMessage>{errors.title?.message}</ErrorMessage>
         <Controller
           name='description'
           control={control}
@@ -71,14 +65,7 @@ const NewIssuePage = () => {
             />
           )}
         />
-        {errors.description && (
-          <Text
-            color='red'
-            as='p'
-          >
-            {errors.description.message}
-          </Text>
-        )}
+        <ErrorMessage>{errors.description?.message}</ErrorMessage>
         <Button className='cursor-pointer'>Submit New Issue</Button>
       </form>
     </div>
